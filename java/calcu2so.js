e160021b38911f41097e4a1202ae9d8fa3b72ec2
phuonglinh1211-2004 <22070421@vnu.edu.vn>
2024-12-06 11:07:44 +0700

    h

---
+++ java/calcu2so.js
@@ -0,0 +1,29 @@
+function calculate(operation) {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    let result;
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                result = 'Error : Cannot divided by 0.';
+            }
+            break;
+        default:
+            result = 'Error';
+            break;
+    }
+
+    document.getElementById('result').innerText = 'result: ' + result;
+}
